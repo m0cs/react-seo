7049a59a7079a77b51c5ef470495d1cacde4fd11
mocs <moco59@gmail.com>
2021-06-01 12:07:58 -0300

    Small fix FE item list query parser.

--- app/src/item-list/itemListRouter.js
+++ app/src/item-list/itemListRouter.js
@@ -4,27 +4,27 @@ import ItemList from './ItemList';
 function ItemListRouter(props) {
   const { search } = useLocation();
   // remove ? and split by separator &
+  let queryValue;
   const queryString = search.replace('?', '').split('&');
-  const searchValue = queryString.find((query) => {
+  queryString.find((query) => {
     const queryArr = query.split('=');
     const key = queryArr[0];
     const value = queryArr[1];
-
     if (key === 'search') {
-      return value;
-    } else {
-      return undefined;
+      queryValue = value;
     }
+
+    return key === 'search';
   });
 
   function handleItemSelect(item) {
     props.onItemSelect(item);
   }
 
-  if (!searchValue) {
+  if (!queryValue) {
     return <div></div>;
   } else {
-    return <ItemList like={searchValue} onItemSelect={handleItemSelect} />;
+    return <ItemList like={queryValue} onItemSelect={handleItemSelect} />;
   }
 }
 
