4b978316164169592d76cfb7f93b89bc76102cc7
mocs <moco59@gmail.com>
2021-06-01 14:03:27 -0300

    Small fix fe decimal style

--- app/src/shared/components/currency/Currency.js
+++ app/src/shared/components/currency/Currency.js
@@ -4,17 +4,23 @@ import './Currency.scss';
 class Currency extends Component {
   // Component methods
   render() {
+    const isBigFormat = this.props.big;
     const price = this.props.price;
-    const bigClass = this.props.big ? 'big' : '';
+    const bigClass = isBigFormat ? 'big' : '';
     const currency = price.currency === 'USD' ? 'U$S' : '$';
     const amount = Math.trunc(price.amount);
     const decimals = price.decimals < 10 ? price.decimals + '0' : price.decimals;
+    const decimalSeparator = !isBigFormat ? <span>.</span> : '';
     return (
       <div className={'Currency-container ' + bigClass}>
-        <span className="Currency-tag">{currency}</span>
-        <span>{amount.toLocaleString()}</span>
-        <span>.{decimals}</span>
-        <span></span>
+        <div>
+          <span className="Currency-tag">{currency}</span>
+          <span>{amount.toLocaleString()}</span>
+        </div>
+        <div className="Currency-decimals-container">
+          {decimalSeparator}
+          <span className="Currency-decimals">{decimals}</span>
+        </div>
       </div>
     );
   }
